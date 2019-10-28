86629cb23d92ec1207ca3eb6ee1769c95c86db6d
Erlb88 <erl_buenbrazo@comcast.net>
2019-10-27 21:51:43 -0700

    adding of calculator.js file for assignement no.2

---
+++ lesson02/calculator.js
@@ -0,0 +1,31 @@
+console.log('adding number, if x=6, and y=12');
+x=6;
+y=12;
+a=x+y;
+b=y/x
+
+console.log('answer is', a);
+
+console.log('subtracting number if x=6, and y=12');
+x=6;
+y=12;
+a=x-y
+console.log('answer is', a);
+
+console.log('multiplying number if x=6, and y=12');
+x=6;
+y=12;
+a=x*y
+console.log('answer is', a);
+
+console.log('dividing number if x=6, and y=12');
+x=6;
+y=12;
+a=x/y
+console.log('answer is', a);
+
+console.log('what is the answer to --(2y+2a)/y');
+x=6;
+y=12;
+v=(2*y+2*x)/y
+console.log('answer is', v);
